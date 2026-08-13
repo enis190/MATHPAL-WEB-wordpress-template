84d722dbb91edab915a35b9229f693815081d495
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-13 04:12:24 +0000

    temp

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -1,68 +1,52 @@
 <?php
-/**
- * The template for displaying product content within loops
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 9.4.0
- */
-
 defined( 'ABSPATH' ) || exit;
 
-global $product;
-
-// Check if the product is a valid WooCommerce product and ensure its visibility before proceeding.
-if ( ! is_a( $product, WC_Product::class ) || ! $product->is_visible() ) {
-	return;
-}
+get_header();
+include(TEMPLATEPATH . '../template-parts/header-default.php');
 ?>
-Elvissssssssssssssssssssssssssssssssssssssssssssss
-<li <?php wc_product_class( '', $product ); ?>>
-	<?php
-	/**
-	 * Hook: woocommerce_before_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_open - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item' );
 
-	/**
-	 * Hook: woocommerce_before_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_show_product_loop_sale_flash - 10
-	 * @hooked woocommerce_template_loop_product_thumbnail - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item_title' );
+<div class="container c-div-padding shop">
+    <div class="row">
+        <div class="col-12">
 
-	/**
-	 * Hook: woocommerce_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
-	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
+            <?php
+            /**
+             * Hook: woocommerce_before_main_content.
+             */
+            do_action( 'woocommerce_before_main_content' );
 
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
+            if ( woocommerce_product_loop() ) {
 
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
-	?>
-</li>
+                do_action( 'woocommerce_before_shop_loop' );
+
+                woocommerce_product_loop_start();
+
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    while ( have_posts() ) {
+                        the_post();
+                        do_action( 'woocommerce_shop_loop' );
+                        wc_get_template_part( 'content', 'product' );
+                    }
+                }
+
+                woocommerce_product_loop_end();
+
+                do_action( 'woocommerce_after_shop_loop' );
+
+            } else {
+                do_action( 'woocommerce_no_products_found' );
+            }
+
+            /**
+             * Hook: woocommerce_after_main_content.
+             */
+            do_action( 'woocommerce_after_main_content' );
+            ?>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include(TEMPLATEPATH . '../template-parts/footer-default.php');
+get_footer();
