f3ed377f9f415ebd87704b00b45bc697c185034f
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-13 05:07:18 +0000

    .

---
+++ woocommerce/archive-product-back.php
@@ -0,0 +1,46 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+get_header();
+include get_template_directory() . '/template-parts/header-default.php';
+?>
+
+<div class="container c-div-padding shop c-woocommerce">
+    <div class="row">
+        <div class="col-12">
+
+            <?php
+            //do_action( 'woocommerce_before_main_content' );
+            do_action( 'woocommerce_shop_loop_header' );
+
+            if ( woocommerce_product_loop() ) {
+
+                do_action( 'woocommerce_before_shop_loop' );
+
+                woocommerce_product_loop_start();
+
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    while ( have_posts() ) {
+                        the_post();
+                        do_action( 'woocommerce_shop_loop' );
+                        wc_get_template_part( 'content', 'product' );
+                    }
+                }
+
+                woocommerce_product_loop_end();
+
+                do_action( 'woocommerce_after_shop_loop' );
+            } else {
+                do_action( 'woocommerce_no_products_found' );
+            }
+
+            //do_action( 'woocommerce_after_main_content' );
+            //do_action( 'woocommerce_sidebar' );
+            ?>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include get_template_directory() . '/template-parts/footer-default.php';
+get_footer();
