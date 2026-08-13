351fd52176eece9372702231e2b2bd8ab9e8ce9e
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-13 01:12:07 +0000

    test-compartir

--- single-post.php
+++ single-post.php
@@ -14,15 +14,34 @@ if (have_posts()) :
         <div class="container-fluid single_blog">
             <div class="container">
                 <div class="row">
-                <div class="col-12">
-                    <h2 class="titulo-h3 fade-in"><?php the_title(); ?></h2>
-                </div>
+                    <div class="col-12">
+                        <h2 class="titulo-h3 fade-in"><?php the_title(); ?></h2>
+                    </div>
                 </div>
 
                 <div class="row">
-                <div class="col-12">
-                        <?php the_content(); ?>
+                    <div class="col-12">
+                            <?php the_content(); ?>
+                    </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-12 mt-4 mb-4">
+                        <!-- Compartir en redes sociales -->
+                         <?php
+                        $url_actual   = urlencode( get_permalink() );
+                        $titulo_post  = urlencode( get_the_title() );
+                        ?>
+                        <div class="compartir-redes">
+                            <span>Compartir:</span>
+                            <!-- Facebook -->
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url_actual; ?>"
+                            target="_blank" rel="noopener noreferrer" aria-label="Compartir en Facebook">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </div>
+                        
+                    </div>
                 </div>
 
             </div>
