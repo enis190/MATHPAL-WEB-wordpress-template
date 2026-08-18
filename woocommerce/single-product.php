c33b3cc5c8d1109cd05303a3b2dac7aa16d6d3d5
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-18 06:07:04 +0000

    .

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -5,6 +5,22 @@ get_header();
 include get_template_directory() . '/template-parts/header-default.php';
 ?>
 
+<div class="container single-product enis-single-product">
+    <div class="row">
+        <div class="col-12">
+
+            <?php
+            do_action( 'woocommerce_before_main_content' );
+
+            while ( have_posts() ) :
+                the_post();
+                wc_get_template_part( 'content', 'single-product' );
+            endwhile;
+            ?>
+
+        </div>
+    </div>
+</div>
 
 <?php
 //get_footer( 'shop' );
