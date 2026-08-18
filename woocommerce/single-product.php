1d6dbba695de47eb05d651315b90a125af0fbc78
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-18 12:30:22 +0000

    .

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -5,21 +5,20 @@ get_header();
 include get_template_directory() . '/template-parts/header-default.php';
 ?>
 
-<div class="container single-product c-woocommerce c-single-product">
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+
     <div class="row">
-        <div class="col-12">
+        <div class="col-6 c-product-gallery">
+            <?php do_action( 'woocommerce_before_single_product_summary' ); ?>
+        </div>
 
-            <?php
-            //do_action( 'woocommerce_before_main_content' );
-
-            while ( have_posts() ) :
-                the_post();
-                wc_get_template_part( 'content', 'single-product' );
-            endwhile;
-            ?>
-
+        <div class="col-6 summary entry-summary">
+            <?php do_action( 'woocommerce_single_product_summary' ); ?>
         </div>
     </div>
+
+    <?php do_action( 'woocommerce_after_single_product_summary' ); ?>
+
 </div>
 
 <?php
