f21a25017dc6d00e5da61aa599e04e823c077525
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-08-28 05:56:03 +0000

    .

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -134,33 +134,60 @@ include get_template_directory() . '/template-parts/header-default.php';
             <div class="col-12">
                 <section class="slider responsive3_shop">
                     <?php
-                    //do_action( 'woocommerce_before_main_content' );
-                    do_action('woocommerce_shop_loop_header');
+                    // //do_action( 'woocommerce_before_main_content' );
+                    // do_action('woocommerce_shop_loop_header');
 
-                    if (woocommerce_product_loop()) {
+                    // if (woocommerce_product_loop()) {
 
-                        do_action('woocommerce_before_shop_loop');
+                    //     do_action('woocommerce_before_shop_loop');
 
-                        woocommerce_product_loop_start();
+                    //     woocommerce_product_loop_start();
 
-                        if (wc_get_loop_prop('total')) {
-                            while (have_posts()) {
-                                the_post();
-                                do_action('woocommerce_shop_loop');
-                                wc_get_template_part('content', 'product');
-                            }
-                        }
+                    //     if (wc_get_loop_prop('total')) {
+                    //         while (have_posts()) {
+                    //             the_post();
+                    //             do_action('woocommerce_shop_loop');
+                    //             wc_get_template_part('content', 'product');
+                    //         }
+                    //     }
 
-                        woocommerce_product_loop_end();
+                    //     woocommerce_product_loop_end();
 
-                        do_action('woocommerce_after_shop_loop');
-                    } else {
-                        do_action('woocommerce_no_products_found');
-                    }
+                    //     do_action('woocommerce_after_shop_loop');
+                    // } else {
+                    //     do_action('woocommerce_no_products_found');
+                    // }
 
-                    //do_action( 'woocommerce_after_main_content' );
-                    //do_action( 'woocommerce_sidebar' );
+                    // //do_action( 'woocommerce_after_main_content' );
+                    // //do_action( 'woocommerce_sidebar' );
                     ?>
+
+                    <?php
+                    $bloque1 = new WP_Query(array(
+                        'post_type'      => 'product',
+                        'post_status'    => 'publish',
+                        'posts_per_page' => -1, // trae todos los de esa categoría; pon un número si quieres limitar
+                        'tax_query'      => array(
+                            array(
+                                'taxonomy' => 'product_cat',
+                                'field'    => 'slug',
+                                'terms'    => array('diagnostic-class'),
+                            ),
+                        ),
+                    ));
+
+                    if ($bloque1->have_posts()) :
+                    ?>
+
+                        <?php
+                        while ($bloque1->have_posts()) :
+                            $bloque1->the_post();
+                            global $product;
+                            wc_get_template_part('content', 'product');
+                        endwhile;
+                        wp_reset_postdata();
+                        ?>
+                    <?php endif; ?>
                 </section>
             </div>
         </div>
