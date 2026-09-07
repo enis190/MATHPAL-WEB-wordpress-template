c2c6b48b7b4bd9a724c51d1ffd96019010945cd6
Elvis Nicolas <elvis.nicolas.santiago@gmail.com>
2026-09-07 11:59:54 +0000

    .

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -313,7 +313,7 @@ include get_template_directory() . '/template-parts/header-default.php';
 
 
 
-<div class="container-fluid c-div-padding shop c-woocommerce c-archive-product home-ruta precios">
+<div class="container-fluid c-div-padding shop c-woocommerce c-archive-product home-ruta precios-old">
     <div class="container0">
         <div class="row">
             <div class="col-12 text-center">
